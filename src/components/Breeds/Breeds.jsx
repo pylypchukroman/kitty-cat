4f0f711f0ee8fb19ac5a07d8b3f77e5b1fb7cf46
pylypchukroman <romanpylypchuk@gmail.com>
2022-08-05 18:13:24 +0300

    add breeds styles

--- src/components/Breeds/Breeds.jsx
+++ src/components/Breeds/Breeds.jsx
@@ -9,7 +9,7 @@ import { getSelectedBreed } from 'utils/CatAPI';
 const Breeds = () => {
   let location = useLocation();
   const [breeds, setBreeds] = useState([]);
-  const [limit, setLimit] = useState('10');
+  const [limit, setLimit] = useState('20');
   const [selectedBreed, setSelectedBreed] = useState('');
   const [selectedBreedList, setSelectedBreedList] = useState([]);
   const curlocation = location.pathname.replace('/', '');
@@ -38,62 +38,66 @@ const Breeds = () => {
   return (
     <>
       <div className={style.navBar}>
-        <HistoryBar currentLocation={curlocation} />
-        <form onSubmit={handleSubmit}>
-          <label>
-            <input
-              type="text"
-              className={style.searchInpute}
-              placeholder="Search for breeds by name"
-              onChange={event => changeBreed(event.target.value)}
-            />
-            <button type="button" className={style.searchInnerBtnWrapper}>
-              <svg className={style.searchInnerBtn}>
-                <use href={sprite + '#icon-search'}></use>
-              </svg>
-            </button>
-          </label>
-        </form>
-        <form>
-          <label>
-            <select
-              className={style.limitSelect}
-              onChange={event => changeLimit(event.target.value)}
-              value={limit}
-            >
-              <option value="5">Limit: 5</option>
-              <option value="10">Limit: 10</option>
-              <option value="15">Limit: 15</option>
-              <option value="20">Limit: 20</option>
-            </select>
-          </label>
-        </form>
+        <div className={style.wrapper}>
+          <HistoryBar currentLocation={curlocation} />
+          <form onSubmit={handleSubmit}>
+            <label>
+              <input
+                type="text"
+                className={style.searchInpute}
+                placeholder="Search for breeds by name"
+                onChange={event => changeBreed(event.target.value)}
+              />
+              <button type="button" className={style.searchInnerBtnWrapper}>
+                <svg className={style.searchInnerBtn}>
+                  <use href={sprite + '#icon-search'}></use>
+                </svg>
+              </button>
+            </label>
+          </form>
+          <form>
+            <label>
+              <select
+                className={style.limitSelect}
+                onChange={event => changeLimit(event.target.value)}
+                value={limit}
+              >
+                <option value="5">Limit: 5</option>
+                <option value="10">Limit: 10</option>
+                <option value="15">Limit: 15</option>
+                <option value="20">Limit: 20</option>
+              </select>
+            </label>
+          </form>
+        </div>
+        <ul className={style.breedsGallery}>
+          {selectedBreedList.length < 1
+            ? breeds.map(breed => (
+                <li key={breed.id} className={style.item}>
+                  <img
+                    className={style.BreedsGalleryImg}
+                    src={breed.image.url}
+                    alt={breed.name}
+                    width="200"
+                    height="200"
+                  />
+                  <p>{breed.id}</p>
+                </li>
+              ))
+            : selectedBreedList.map(selectedOneBreed => (
+                <li key={selectedOneBreed.id} className={style.item}>
+                  <img
+                    className={style.BreedsGalleryImg}
+                    src={selectedOneBreed.url}
+                    alt={selectedOneBreed.breeds[0].id}
+                    width="200"
+                    height="200"
+                  />
+                  <p>{selectedOneBreed.breeds[0].id}</p>
+                </li>
+              ))}
+        </ul>
       </div>
-      <ul className={style.breedsGallery}>
-        {selectedBreedList.length < 1
-          ? breeds.map(breed => (
-              <li key={breed.id}>
-                <img
-                  src={breed.image.url}
-                  alt={breed.name}
-                  width="200"
-                  height="200"
-                />
-                <p>{breed.id}</p>
-              </li>
-            ))
-          : selectedBreedList.map(selectedOneBreed => (
-              <li key={selectedOneBreed.id}>
-                <img
-                  src={selectedOneBreed.url}
-                  alt={selectedOneBreed.breeds[0].id}
-                  width="200"
-                  height="200"
-                />
-                <p>{selectedOneBreed.breeds[0].id}</p>
-              </li>
-            ))}
-      </ul>
     </>
   );
 };
